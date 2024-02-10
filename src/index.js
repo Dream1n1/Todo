6551bdfd071450d47614b81e81295b52b4413727
H.Nasri <hamzanasri156@gmail.com>
2024-02-10 18:03:21 +0100

    add task class

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export class todo_item {
+    constructor(title, due_date, note, check) {
+        this.title = title,
+        this.due_date = due_date,
+        this.note = note,
+        this.check = "uncheck"
+    }
+}
