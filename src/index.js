c4779dfad4c935cbc44af93c87e3f6a43f54302b
H.Nasri <hamzanasri156@gmail.com>
2024-02-11 13:20:23 +0100

    add project and task creation functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-export class task {
+// let projects_list = [[task1,task2,task3],[],[]...];
+let projects_list = [];
+
+function createProject() {
+    let project = [];
+    projects_list.push(project);
+}
+
+class task {
     constructor(title, due_date, note, check) {
         this.title = title,
         this.due_date = due_date,
         this.note = note,
         this.check = "uncheck"
     }
+}
+
+function createTask(project, title, due_date, note, check) {
+    project.push(new task(title, due_date, note, check));
 }
