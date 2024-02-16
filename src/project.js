e0598d4bfc4a369c747ce5320d79828872d96001
H.Nasri <hamzanasri156@gmail.com>
2024-02-16 18:18:12 +0100

    move task_content_creator() to it's own module

--- src/project.js
+++ src/project.js
@@ -1,4 +1,5 @@
-import {projects_list, task, createProject, createTask} from './task.js';
+import { projects_list, task, createProject, createTask } from './task.js';
+import { task_content_creator } from "./task_list_builder.js";
 
 const projectNameInput = document.querySelector('.project_name_input');
 const projectList = document.querySelector('.projectList');
@@ -47,7 +48,7 @@ function validate() {
                 removeAllChildNodes(tasks_container);
                 //create associated list of tasks and show them on screen
                 projects_list[current_project_id].forEach(element => {
-                    task_content_creator(element.title, element.due_date, element.note, current_project_id, element);
+                    task_content_creator(projects_list, element.title, element.due_date, element.note, current_project_id, element);
                 });
                 return current_project_id;
             }
@@ -63,56 +64,9 @@ function task_on_screen() {
         return;
     } else {
         let new_task = createTask(current_project_id, task_name_input.value, task_date_input.value, task_note_input.value);
-        task_content_creator(task_name_input.value, task_date_input.value, task_note_input.value, current_project_id, new_task);
+        task_content_creator(projects_list, task_name_input.value, task_date_input.value, task_note_input.value, current_project_id, new_task);
     }
 }
-
-function task_content_creator(get_task_name, get_task_date, get_task_note, projectIndex, currentTask) {
-    let task_item = document.createElement('div');
-    task_item.className = 'task_item';
-    
-    let taskIndex = projects_list[projectIndex].indexOf(currentTask);
-    if (projects_list[projectIndex][taskIndex].check == "checked") {
-        task_check.checked = true;
-        task_item.style.textDecoration = "line-through";
-    } else {
-        task_item.style.textDecoration = "none";
-    }
-    let  task_check= document.createElement('input');
-    task_check.setAttribute('type','checkbox');
-    task_check.setAttribute('id','task_check');
-    task_check.addEventListener('click', ()=> {//make a function outside and run it here for readability
-        if (task_check.checked == true) {
-            projects_list[projectIndex][taskIndex].check = "checked";
-            console.log(projects_list[projectIndex]);
-            task_item.style.textDecoration = "line-through";
-        } else {
-            projects_list[projectIndex][taskIndex].check = "uncheck";
-            task_item.style.textDecoration = "none";
-
-        }
-    })
-    
-    let  task_name= document.createElement('div');
-    task_name.className = 'task_name';
-    task_name.innerHTML = get_task_name;
-    
-    let  task_date= document.createElement('div');
-    task_date.className = 'task_date';
-    task_date.innerHTML = get_task_date;
-
-    let  task_note= document.createElement('div');
-    task_note.className = 'task_note';
-    task_note.innerHTML = get_task_note;
-    
-    task_item.appendChild(task_check);
-    task_item.appendChild(task_name);
-    task_item.appendChild(task_date);
-    task_item.appendChild(task_note);
-
-    tasks_container.appendChild(task_item);
-}
-
 
 
 export {validate, task_on_screen};
