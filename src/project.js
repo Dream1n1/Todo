c54da44306ca57f996fd9bd6c57528fca339b183
H.Nasri <hamzanasri156@gmail.com>
2024-02-18 22:07:38 +0100

    Add default project and task
    Moved code that builds projects and tasks to their new functions to be able to add the default project and task,
    and for better readability.

--- src/project.js
+++ src/project.js
@@ -1,5 +1,5 @@
 import { projects_list, createProject, createTask } from './task.js';
-import { task_content_creator } from "./task_list_builder.js";
+import { task_content_creator } from "./task_content.js";
 import { removeAllChildNodes } from "./removeChildNodes.js";
 
 const projectNameInput = document.querySelector('.project_name_input');
@@ -14,49 +14,62 @@ const tasks_container= document.querySelector('.tasks');
 let project_id_counter = 0;
 let current_project_id;
 let previous_project_id;
+function default_project_build(project_name) {
+    createProject();
+    removeAllChildNodes(tasks_container);
+    
 
-// this creates a new project and gives an id to it
+    let newProject = document.createElement('div');
+    newProject.className = 'project';
+    newProject.setAttribute('id', project_id_counter.toString());
+    newProject.innerHTML = project_name;
+    
+    previous_project_id = project_id_counter;
+    current_project_id = project_id_counter;
+    newProject.addEventListener('click', (event)=>{
+        previous_project_id = current_project_id;
+        current_project_id = parseInt(event.target.id);
+        if (previous_project_id == current_project_id) {
+            return;
+        } else {
+            removeAllChildNodes(tasks_container);
+            //create associated list of tasks and show them on screen
+            projects_list[current_project_id].forEach(element => {
+                task_content_creator(projects_list, element.title, element.due_date, element.note, current_project_id, element);
+            });
+            return current_project_id;
+        }
+    });
+    projectList.appendChild(newProject);
+    project_id_counter++;
+    return current_project_id;
+}
+
+function default_task_build(task_name, task_date, task_note) {
+    let new_task = createTask(current_project_id, task_name, task_date, task_note);
+    task_content_creator(projects_list, task_name, task_date, task_note, current_project_id, new_task);
+}
+
+default_project_build("Important");
+default_task_build("Workout", "18/02/2024", "Leg day");
+
 function project_on_screen() {
     if (projectNameInput.value == "") {
         return;
     } else {
-        createProject();
-        removeAllChildNodes(tasks_container);
-
-        let newProject = document.createElement('div');
-        newProject.className = 'project';
-        newProject.setAttribute('id', project_id_counter.toString());
-        newProject.innerHTML = projectNameInput.value;
+        default_project_build(projectNameInput.value);
         projectNameInput.value = "";
-        
-        previous_project_id = project_id_counter;
-        current_project_id = project_id_counter;
-        newProject.addEventListener('click', (event)=>{
-            previous_project_id = current_project_id;
-            current_project_id = parseInt(event.target.id);
-            if (previous_project_id == current_project_id) {
-                return;
-            } else {
-                removeAllChildNodes(tasks_container);
-                //create associated list of tasks and show them on screen
-                projects_list[current_project_id].forEach(element => {
-                    task_content_creator(projects_list, element.title, element.due_date, element.note, current_project_id, element);
-                });
-                return current_project_id;
-            }
-        });
-        projectList.appendChild(newProject);
-        project_id_counter++;
     }
-    return current_project_id;
 }
             
 function task_on_screen() {
-    if (!task_name_input.value|| !task_date_input.value) {
+    if (!task_name_input.value || !task_date_input.value) {
         return;
     } else {
-        let new_task = createTask(current_project_id, task_name_input.value, task_date_input.value, task_note_input.value);
-        task_content_creator(projects_list, task_name_input.value, task_date_input.value, task_note_input.value, current_project_id, new_task);
+        default_task_build(task_name_input.value, task_date_input.value, task_note_input.value);
+        task_name_input.value = "";
+        task_date_input.value = "";
+        task_note_input.value = "";
     }
 }
 
